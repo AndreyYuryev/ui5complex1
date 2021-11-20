0ec5c5ea1f86067f95fe4972a694c54a13d76506
Andrey Yuryev <andrey.yuryev@t-systems.com>
2021-11-20 13:33:57 +0000

    splitreuseapp: init repo

---
+++ splitreuseapp/webapp/controller/Details.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("ns.splitreuseapp.controller.Details", {
+			onInit: function () {
+                this.getOwnerComponent().getRouter().getRoute("RouteDetails").attachPatternMatched(this.onPatternMatched, this);
+            },
+            
+            onPatternMatched: function(oEvent){
+                let oArguments = oEvent.getParameter('arguments');
+                this.getView().bindElement(this.getView().getModel().createKey('/EventSet', { EventId: oArguments.id} ));
+            }
+		});
+	});
---
+++ splitreuseapp/webapp/controller/Master.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("ns.splitreuseapp.controller.Master", {
+			onInit: function () {
+
+            },
+            
+            onItemPress: function(oEvent) {
+                let oObject = oEvent.getSource().getBindingContext().getObject();
+                this.getOwnerComponent().getRouter().navTo('RouteDetails', {
+                    id: oObject.EventId,
+                })
+            }
+		});
+	});
